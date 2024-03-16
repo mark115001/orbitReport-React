22ced131dba800ff2ae2f86c8851bfafad675997
mark115001 <145294877+mark115001@users.noreply.github.com>
2024-03-16 10:16:27 -0500

    Initil Working AutoGraded 6

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,18 @@
 import Buttons from "./components/Buttons";
 import Table from "./components/Table";
+import Banner from "./components/Banner"
+import { useState } from "react";
+import satData from "./components/satData"
 
 
 function App() {
+  const [sat, setSat] = useState(satData);
+  const displaySats = [...new Set(satData.map((data) => data.orbitType))];
+
   return (
     <div>
       <Buttons />
+      <Banner />
       <Table />
     </div>
   );
