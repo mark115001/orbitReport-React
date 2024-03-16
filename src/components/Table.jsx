8d83e837690821b186933dc909e4ffb2acc5acea
mark115001 <145294877+mark115001@users.noreply.github.com>
2024-03-16 10:41:00 -0500

    Autograded Project 6.1

--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -1,17 +1,19 @@
 const Table = ({ sat }) => {
   return (
     <table>
-    <thead>
-     <tr>
-       <th>Header TBD</th>
-     </tr>
-     </thead>
-     <tbody>
-     <tr>
-       <td>Row Data TBD</td>
-     </tr>
-     </tbody>
-   </table>
+      <thead>
+        <tr>
+          <th>Name</th>
+          <th>Type of Satellite</th>
+          <th>LaunchDate</th>
+          <th>Status</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+        </tr>
+      </tbody>
+    </table>
   );
 };
 
